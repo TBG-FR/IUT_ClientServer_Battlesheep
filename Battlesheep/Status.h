6ce5b398c7c832df337275da36dd9b06a45bc94e
Tom-Brian Garcia <tom-brian.garcia@etu.univ-lyon1.fr>
2018-03-28 00:23:26 +0200

    Create Status.h

---
+++ Battlesheep/Status.h
@@ -0,0 +1,18 @@
+#ifndef BATTLESHIP_STATUS
+
+/* TODO : Improve that ENUM */
+
+enum Status {
+
+  NONE,
+  ONE_PLAYER,
+  GAME_CREATED,
+  GAME_CONFIGURED,
+  TWO_PLAYERS,
+  INGAME,
+  ENDGAME,
+  GAME_PAUSED,
+  
+};
+
+#endif
